97a0f34cdcc9c80c2eaa31a8a63398d4577e0903
Raja-V <rsekhar.vai@gmail.com>
2022-09-18 18:12:10 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+# setup.py command | New command
+# setup.py sdist   | python - m build
+# setup.py test	   | pytest
+# setup.py install | pip install
+# setup.py develop | pip install - e
+# For creating documentation
+#   mkdocs build
+#   mkdocs serve
+#   mkdocs gh-deploy  (for deploying in Github as Github pages)
+# Pre-Commit Hooks
+from setuptools import setup
+
+
+def main() -> None:
+    with open("requirements.txt") as fp:
+        install_requires = fp.read().strip().split("\n")
+
+    metadata = dict(
+        install_requires=install_requires
+    )
+    setup(**metadata)
+
+
+if __name__ == "__main__":
+    main()
